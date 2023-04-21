5f3ff5680d5fade059cbe299948997e463ca5e3c
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-04-21 15:16:22 +0300

    completed assignments

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -1,29 +1,14 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import About from "../components/About";
+import React from "react";
 
-test("renders a <aside> element", () => {
-  const { container } = render(<About />);
-  expect(container.querySelector("aside")).toBeInTheDocument();
-});
+function About({ image, about }) {
+  const imageUrl = image || "https://via.placeholder.com/215";
+  return (
+    <aside>
+      <img src={imageUrl} alt="blog logo" />
+      <p>{about}</p>
+    </aside>
+  );
+}
 
-test("renders a <img> with the blog logo and alt text of 'blog logo'", () => {
-  render(<About image="logo.svg" />);
-  const img = screen.queryByAltText("blog logo");
-  expect(img).toBeInTheDocument();
-  expect(img.src).toContain("logo.svg");
-});
+export default About;
 
-test("uses a default value for the image if no image is passed as a prop", () => {
-  render(<About />);
-  const img = screen.queryByAltText("blog logo");
-  expect(img).toBeInTheDocument();
-  expect(img.src).toContain("https://via.placeholder.com/215");
-});
-
-test("renders a <p> with the about text", () => {
-  render(<About about="About this blog" />);
-  const p = screen.queryByText("About this blog");
-  expect(p).toBeInTheDocument();
-  expect(p.tagName).toBe("P");
-});
--- src/__tests__/ArticleList.test.js
+++ src/__tests__/ArticleList.test.js
@@ -1,34 +1,16 @@
-import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import ArticleList from "../components/ArticleList";
+import React from 'react';
+import Article from './Article';
 
-const posts = [
-  {
-    id: 1,
-    title: "Components 101",
-    date: "December 15, 2020",
-    preview: "Setting up the building blocks of your site",
-  },
-  {
-    id: 2,
-    title: "React Data Flow",
-    date: "December 11, 2020",
-    preview: "Passing props is never passé",
-  },
-  {
-    id: 3,
-    title: "Function Components vs Class Components",
-    date: "December 10, 2020",
-    preview: "React, meet OOJS.",
-  },
-];
+function ArticleList(props) {
+  const { articles } = props;
 
-test("renders a <main> element", () => {
-  const { container } = render(<ArticleList posts={posts} />);
-  expect(container.querySelector("main")).toBeInTheDocument();
-});
+  return (
+    <main>
+      {articles.map((article) => (
+        <Article key={article.id} article={article} />
+      ))}
+    </main>
+  );
+}
 
-test("renders a Article component for each post passed as a prop", () => {
-  const { container } = render(<ArticleList posts={posts} />);
-  expect(container.querySelector("main").children).toHaveLength(3);
-});
+export default ArticleList;
